51d854c32e7a494d44d0866dd9e5b9305b4620c7
Cyberadmin <jahparadise1@gmail.com>
2024-12-21 01:09:53 -0800

    prisma seed.ts backend paraphrase

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 import path from "path";
+
 const prisma = new PrismaClient();
 
 async function deleteAllData(orderedFileNames: string[]) {
